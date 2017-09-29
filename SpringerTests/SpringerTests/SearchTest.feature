ab81d4e4cc00591f9275afe975cafa7097a86e76
haythamelzahar <haytham.elzahar@asos.com>
2017-09-29 10:53:48 +0100

    SpringTest

---
+++ SpringerTests/SpringerTests/SearchTest.feature
@@ -0,0 +1,11 @@
+﻿Feature: SearchTest
+	In order to check the Search is working
+	As a user
+	I want to be given Search results correctly
+
+@mytag
+Scenario: Testing the Search bar
+	Given I am a user
+	And I navigate to the site
+	When I search for the word
+	Then I receive search results with the word in the title
---
+++ SpringerTests/SpringerTests/SearchTestSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpringerTests
+{
+    [Binding]
+    public class SearchTestSteps
+    {
+        [Given(@"I am a user")]
+        public void GivenIAmAUser()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I navigate to the site")]
+        public void GivenINavigateToTheSite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I search for the word")]
+        public void WhenISearchForTheWord()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I receive search results with the word in the title")]
+        public void ThenIReceiveSearchResultsWithTheWordInTheTitle()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
